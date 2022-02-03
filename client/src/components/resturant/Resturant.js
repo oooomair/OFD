6050b3ba959c5a44e240166c023397c0f48a4571
oooomair <omairsalam123@gmail.com>
2022-02-04 00:42:23 +0400

    resturant finished

--- client/src/components/resturant/Resturant.js
+++ client/src/components/resturant/Resturant.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ResturantLand from './resturantComponents/ResturantLand';
 import './resturant.scss'
+import ResturantFoods from './resturantComponents/ResturantFoods';
 
 const Resturant = () => {
   return <div className='resturant' >
       <ResturantLand/>
+      <ResturantFoods/>
   </div>;
 };
 
---
+++ client/src/components/resturant/resturantComponents/ResturantFoods.js
@@ -0,0 +1,15 @@
+import ResturantFood from './ResturantFood';
+
+const ResturantFoods = () => {
+  return <div className='resturant__resturant-foods' >
+      <ResturantFood/>
+      <ResturantFood/>
+      <ResturantFood/>
+      <ResturantFood/>
+      <ResturantFood/>
+      <ResturantFood/>
+
+  </div>;
+};
+
+export default ResturantFoods;
